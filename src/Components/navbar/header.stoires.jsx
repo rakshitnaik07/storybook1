e9340fbefeba75c1b430480c840ebb660de7f46b
rakshit <rakshitnagaraj951@gmail.com>
2025-02-12 23:13:24 +0530

    first-commit

---
+++ src/Components/navbar/header.stoires.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./header";
+
+export default{
+    title: 'Components/NavBar/Header',
+    Component:Header,
+  };
+  
+
+export const Default=()=><Header/>
